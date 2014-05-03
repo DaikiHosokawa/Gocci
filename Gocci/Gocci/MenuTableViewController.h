ae161d8b7645a4c39cf0111ba9dedb815c447465
Daiki Hosokawa <popeppo4@gmail.com>
2014-05-03 14:03:19 +0900

    second commit

---
+++ Gocci/Gocci/MenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuTableViewController.h
+//  Gocci
+//
+//  Created by Daiki Hosokawa on 2014/05/03.
+//  Copyright (c) 2014年 Massara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuTableViewController : UITableViewController
+
+@end
