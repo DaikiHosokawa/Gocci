c3bdb717a964c453510a652b093b6af84b44962a
Daiki Hosokawa <popeppo4@gmail.com>
2015-02-28 13:29:19 +0900

    マージを変更

--- Gocci/Classes/Timeline/TimelineTableViewController.m
+++ Gocci/Classes/Timeline/TimelineTableViewController.m
@@ -526,7 +526,7 @@ static NSString * const SEGUE_GO_EVERY_COMMENT = @"goEveryComment";
     TimelineCell *currentCell = [TimelineCell cell];
     [currentCell configureWithTimelinePost:self.posts[[self _currentIndexPath].row]];
     CGRect movieRect = CGRectMake((self.tableView.frame.size.width - currentCell.thumbnailView.frame.size.width) / 2,
-                                  currentHeight + currentCell.thumbnailView.frame.origin.y-2,
+                                  currentHeight + currentCell.thumbnailView.frame.origin.y,
                                   currentCell.thumbnailView.frame.size.width,
                                   currentCell.thumbnailView.frame.size.height);
     
