d6c877afdac8c1d66376535be58d92317eb81790
Toshiaki Shou <akira@dezamisystem.com>
2015-05-10 00:10:08 +0900

    テスト用

---
+++ Gocci/Classes/Recorder/DummyViewController.h
@@ -0,0 +1,13 @@
+//
+//  DummyViewController.h
+//  Gocci
+//
+//  Created by デザミ on 2015/05/10.
+//  Copyright (c) 2015年 Massara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DummyViewController : UIViewController
+
+@end
