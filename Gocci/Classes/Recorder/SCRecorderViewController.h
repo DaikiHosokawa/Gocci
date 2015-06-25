92661dedd153e7ca59336224de74161e4e86246e
Daiki Hosokawa <popeppo4@gmail.com>
2015-06-25 18:03:41 +0900

    Venderの追加

--- Gocci/Classes/Recorder/SCRecorderViewController.h
+++ Gocci/Classes/Recorder/SCRecorderViewController.h
@@ -14,6 +14,7 @@
 #import "DemoContentView.h"
 #import "SCFirstView.h"
 #import "SCSecondView.h"
+#import <FBSDKShareKit/FBSDKShareKit.h>
 
 
 @interface SCRecorderViewController : UIViewController<SCRecorderDelegate, SCFirstViewDelegate,SCSecondViewDelegate,UIAlertViewDelegate>{
