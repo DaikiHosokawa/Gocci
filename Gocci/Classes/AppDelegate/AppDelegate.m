e45ab7652c383e85e2fba5f955f84ed49a27b745
Markus Wanke <m-wanke@inase-inc.jp>
2015-09-08 15:21:34 +0900

    Twitter login implemented as 'Proof of concept'

--- Gocci/Classes/AppDelegate/AppDelegate.m
+++ Gocci/Classes/AppDelegate/AppDelegate.m
@@ -18,6 +18,8 @@
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
 
+#import <Fabric/Fabric.h>
+#import <TwitterKit/TwitterKit.h>
 
 @interface AppDelegate() {
     UITabBarController *tabBarController;
@@ -96,6 +98,15 @@
 #ifndef INDEVEL
     [Crittercism enableWithAppID: CRITTERCISM_APP_ID];
 #endif
+    
+    // twitter aids
+    [[Twitter sharedInstance] startWithConsumerKey:TWITTER_CONSUMER_KEY consumerSecret:TWITTER_CONSUMER_SECRET];
+    [Fabric with:@[[Twitter sharedInstance]]];
+
+    
+    
+    
+    
     [GMSServices provideAPIKey: GOOGLE_MAP_SERVICE_API_KEY];
     
     //3.5inchと4inchを読み分けする
