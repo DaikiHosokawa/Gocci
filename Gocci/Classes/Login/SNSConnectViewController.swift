58e9c792be0712d50f9efa320058cc059ebe1c64
Markus Wanke <m-wanke@inase-inc.jp>
2015-09-25 19:13:47 +0900

    Pre translation labels + button logic for sons connection (untested o_0)

--- Gocci/Classes/Login/SNSConnectViewController.swift
+++ Gocci/Classes/Login/SNSConnectViewController.swift
@@ -13,16 +13,33 @@ import UIKit
 
 class SNSConnectViewController : UIViewController {
 
+    @IBOutlet weak var facebookButton: UIButton!
+    
+    @IBOutlet weak var twitterButton: UIButton!
+    
+    @IBOutlet weak var continueButtonCaption: UIButton!
+    
+    var facebookConnectionSuccessful: Bool = false
+    var twitterConnectionSuccessful: Bool = false
+
+    
+    
     @IBAction func facebookConnectClicked(sender: AnyObject) {
         SNSUtil.singelton.connectWithFacebook { (result) -> Void in
             switch result {
                 case .SNS_CONNECTION_SUCCESS:
+                    Util.popup("facebook account connected!!")
+                    self.facebookConnectionSuccessful = true
+                    self.facebookButton.enabled = false
+                    self.transit()
                     break
                 case .SNS_CONNECTION_UNKNOWN_FAILURE:
+                    Util.popup("something really bad happend")
                     break
                 case .SNS_CONNECTION_CANCELED:
                     break
                 case .SNS_PROVIDER_FAIL:
+                    Util.popup("facebook is down")
                     break
             }
             print("=== RESUTLT: \(result)")
@@ -33,15 +50,35 @@ class SNSConnectViewController : UIViewController {
         SNSUtil.singelton.connectWithTwitter(self) { (result) -> Void in
             switch result {
                 case .SNS_CONNECTION_SUCCESS:
+                    Util.popup("twitter account connected!!")
+                    self.twitterConnectionSuccessful = true
+                    self.twitterButton.enabled = false
+                    self.transit()
                     break
+                
                 case .SNS_CONNECTION_UNKNOWN_FAILURE:
+                    Util.popup("something really bad happend")
                     break
                 case .SNS_CONNECTION_CANCELED:
                     break
                 case .SNS_PROVIDER_FAIL:
+                    Util.popup("twitter down")
                     break
             }
             print("=== RESUTLT: \(result)")
+        }
+    }
+    
+    func transit() {
+        if twitterConnectionSuccessful && facebookConnectionSuccessful {
+            let tutorialViewController = self.storyboard!.instantiateViewControllerWithIdentifier("timeLineEntry")
+            self.presentViewController(tutorialViewController, animated: true, completion: nil)
+        }
+        else if twitterConnectionSuccessful {
+            continueButtonCaption.setTitle("continue without facebook connection", forState: UIControlState.Normal)
+        }
+        else if facebookConnectionSuccessful {
+            continueButtonCaption.setTitle("continue without twitter connection", forState: UIControlState.Normal)
         }
     }
     
