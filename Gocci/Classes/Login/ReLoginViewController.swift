3c639f4bbec73de6fcec243c060f073c35d227d7
Daiki Hosokawa <popeppo4@gmail.com>
2015-09-27 12:20:17 +0900

    translation label in ReLoginViewController and change Bitcode status

--- Gocci/Classes/Login/ReLoginViewController.swift
+++ Gocci/Classes/Login/ReLoginViewController.swift
@@ -31,40 +31,37 @@ class ReLoginViewController : UIViewController {
 
     @IBAction func facebookLoginClicked(sender: AnyObject) {
         SNSUtil.singelton.loginWithFacebook() { (result) -> Void in
-            // TODO msg to the user
+            
             switch result {
             case SNSUtil.LoginResult.SNS_LOGIN_SUCCESS:
                 let tutorialViewController = self.storyboard!.instantiateViewControllerWithIdentifier("timeLineEntry")
                 self.presentViewController(tutorialViewController, animated: true, completion: nil)
             case SNSUtil.LoginResult.SNS_LOGIN_UNKNOWN_FAILURE:
-                Util.popup("連携に失敗しました。アカウント情報を再度お確かめください。")
+                Util.popup("再ログインに失敗しました。アカウント情報を再度お確かめください。")
             case SNSUtil.LoginResult.SNS_LOGIN_CANCELED:
                 break
             case SNSUtil.LoginResult.SNS_USER_NOT_REGISTERD:
-                // FIXME JAPANESE NEEDED HOSOKAWA
-                Util.popup("You dont have a registerd gocci account connected with that FACEBOOK account")
+                Util.popup("お使いのFacebookアカウントではまだGocciが登録されていません")
             case SNSUtil.LoginResult.SNS_PROVIDER_FAIL:
-                Util.popup("Facebook連携が現在実施できません。大変申し訳ありません。")
+                Util.popup("再ログインに失敗しました。Facebookが現在使用できません。大変申し訳ありません。")
             }
         }
     }
     
     @IBAction func twitterLoginClicked(sender: AnyObject) {
         SNSUtil.singelton.loginWithTwitter(self) { (result) -> Void in
-            // TODO msg to the user
             switch result {
                 case SNSUtil.LoginResult.SNS_LOGIN_SUCCESS:
                     let tutorialViewController = self.storyboard!.instantiateViewControllerWithIdentifier("timeLineEntry")
                     self.presentViewController(tutorialViewController, animated: true, completion: nil)
                 case SNSUtil.LoginResult.SNS_LOGIN_UNKNOWN_FAILURE:
-                    Util.popup("連携に失敗しました。アカウント情報を再度お確かめください。")
+                    Util.popup("再ログインに失敗しました。アカウント情報を再度お確かめください。")
                 case SNSUtil.LoginResult.SNS_LOGIN_CANCELED:
                     break
                 case SNSUtil.LoginResult.SNS_USER_NOT_REGISTERD:
-                    // FIXME JAPANESE NEEDED HOSOKAWA
-                    Util.popup("You dont have a registerd gocci account connected with that TWITTER account")
+                    Util.popup("お使いのTwitterアカウントではまだGocciが登録されていません")
                 case SNSUtil.LoginResult.SNS_PROVIDER_FAIL:
-                    Util.popup("Facebook連携が現在実施できません。大変申し訳ありません。")
+                    Util.popup("再ログインに失敗しました。Twitterが現在使用できません。大変申し訳ありません。")
 
             }
         }
