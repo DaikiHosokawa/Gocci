d47b7b3ec9d7b06c165bab3d579d8b1457fa95df
Daiki Hosokawa <popeppo4@gmail.com>
2015-10-02 18:00:27 +0900

    Profile page編集中

---
+++ Gocci/Classes/Profile/Colloction/STCustomCollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  STCustomCollectionViewCell.m
+//  UICollectionViewSample
+//
+//  Created by EIMEI on 2013/06/20.
+//  Copyright (c) 2013 stack3.net. All rights reserved.
+//
+
+#import "STCustomCollectionViewCell.h"
+
+@implementation STCustomCollectionViewCell
+
+//
+// readonly property still need to define synthesize.
+//
+@synthesize thumb = _thumb;
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    self.selectedBackgroundView = [[UIView alloc] initWithFrame:self.bounds];
+}
+
+@end
