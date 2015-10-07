8ad96ff8ffd71550f8a20bb5feba2526c49d6de9
Markus Wanke <m-wanke@inase-inc.jp>
2015-10-07 18:36:21 +0900

    Proof of concept liquid button implementation.

--- Gocci/Classes/Timeline/All/TempLiquidButton.swift
+++ Gocci/Classes/Timeline/All/TempLiquidButton.swift
@@ -8,9 +8,50 @@
 
 import Foundation
 
-@objc class LBTest {
+@objc class LiquidButtonWrapper : NSObject, LiquidFloatingActionButtonDataSource, LiquidFloatingActionButtonDelegate {
     
-    class func runlb(cells: ) {
+    var cells: [LiquidFloatingCell] = []
+    var floatingActionButton: LiquidFloatingActionButton!
+
+    func addLiquidButton(vc: UIViewController) {
         
+        //        self.view.backgroundColor = UIColor(red: 55 / 255.0, green: 55 / 255.0, blue: 55 / 255.0, alpha: 1.0)
+        // Do any additional setup after loading the view, typically from a nib.
+        let createButton: (CGRect, LiquidFloatingActionButtonAnimateStyle) -> LiquidFloatingActionButton = { (frame, style) in
+            let floatingActionButton = LiquidFloatingActionButton(frame: frame)
+            floatingActionButton.animateStyle = style
+            floatingActionButton.dataSource = self
+            floatingActionButton.delegate = self
+            return floatingActionButton
+        }
+        
+        let cellFactory: (String) -> LiquidFloatingCell = { (iconName) in
+            return LiquidFloatingCell(icon: UIImage(named: iconName)!)
+        }
+        cells.append(cellFactory("icon_record_cam"))
+        cells.append(cellFactory("icon_record_cam"))
+        cells.append(cellFactory("icon_record_cam"))
+        
+        let floatingFrame = CGRect(x: vc.view.frame.width - 56 - 16, y: vc.view.frame.height - 56 - 16, width: 56, height: 56)
+        let bottomRightButton = createButton(floatingFrame, .Up)
+        
+        let floatingFrame2 = CGRect(x: 16, y: 16, width: 56, height: 56)
+        let topLeftButton = createButton(floatingFrame2, .Down)
+        
+        vc.view.addSubview(bottomRightButton)
+        vc.view.addSubview(topLeftButton)
+    }
+    
+    func numberOfCells(liquidFloatingActionButton: LiquidFloatingActionButton) -> Int {
+        return cells.count
+    }
+    
+    func cellForIndex(index: Int) -> LiquidFloatingCell {
+        return cells[index]
+    }
+    
+    func liquidFloatingActionButton(liquidFloatingActionButton: LiquidFloatingActionButton, didSelectItemAtIndex index: Int) {
+        print("did Tapped! \(index)")
+        liquidFloatingActionButton.close()
     }
 }
