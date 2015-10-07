26e518038305c2cbe34c493f3f5278355b5ab62b
Daiki Hosokawa <popeppo4@gmail.com>
2015-10-07 18:05:20 +0900

    add TempLiquidButton

---
+++ Gocci/Classes/Timeline/All/TempLiquidButton.swift
@@ -0,0 +1,16 @@
+//
+//  TempLiquidButton.swift
+//  Gocci
+//
+//  Created by Castela on 2015/10/07.
+//  Copyright © 2015年 Massara. All rights reserved.
+//
+
+import Foundation
+
+@objc class LBTest {
+    
+    class func runlb(cells: ) {
+        
+    }
+}
