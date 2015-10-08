f2b694138e570f5996be0985995b18fabd771301
Markus Wanke <m-wanke@inase-inc.jp>
2015-10-08 22:21:59 +0900

    SplashViewController rewritten in Swift

---
+++ Gocci/Classes/Login/SplashViewController.swift
@@ -0,0 +1,57 @@
+//
+//  SplashViewController.swift
+//  Gocci
+//
+//  Created by Markus Wanke on 08.10.15.
+//  Copyright © 2015 Massara. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SplashViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        let handleNetOpResult: (NetOpResult, String!) -> Void = { (code, msg) -> Void in
+            switch code {
+            case NetOpResult.NETOP_SUCCESS:
+                AWS2.connectToBackEndWithUserDefData().continueWithBlock({ (task) -> AnyObject! in
+
+                    // TODO maybe predownload something here? first video or so
+
+                    Util.sleep(Int(SPLASH_TIME)) {
+                        self.performSegueWithIdentifier("goTimeline", sender: self)
+                    }
+                    return nil
+                })
+                
+                // TODO
+            case NetOpResult.NETOP_NETWORK_ERROR:
+                break
+            case NetOpResult.NETOP_IDENTIFY_ID_NOT_REGISTERD:
+                break
+            default:
+                break
+                
+            }
+        }
+        
+        
+        if let iid = Util.getUserDefString("iid") {            NetOp.loginWithIID(iid, andThen: handleNetOpResult)
+        }
+        else if let un = Util.getUserDefString("username"), ava = Util.getUserDefString("avatarLink") {
+            NetOp.loginWithAPIV2Conversation(un, avatar: ava, andThen: handleNetOpResult)
+        }
+        else {
+            let twc = self.storyboard?.instantiateViewControllerWithIdentifier("Tutorial")
+            self.presentViewController(twc!, animated: true, completion: nil)
+        }
+
+    }
+}
+
+
+
