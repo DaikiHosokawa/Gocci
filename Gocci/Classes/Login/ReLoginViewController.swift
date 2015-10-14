ff11a2403d8a07247ec73ee96d29e426dfca95e0
Markus Wanke <m-wanke@inase-inc.jp>
2015-10-14 11:26:59 +0900

    Static position for the Liquid Floating Button. (Some Facebook share code that doesn't work is included as well)

--- Gocci/Classes/Login/ReLoginViewController.swift
+++ Gocci/Classes/Login/ReLoginViewController.swift
@@ -56,38 +56,38 @@ class ReLoginViewController : UIViewController {
     }
 
     @IBAction func facebookLoginClicked(sender: AnyObject) {
-        SNSUtil.singelton.loginWithFacebook(currentViewController: self) { (result) -> Void in
+        SNSUtil.loginWithFacebook(currentViewController: self) { (result) -> Void in
             print("Result: " + String(result))
             switch result {
-                case SNSUtil.LoginResult.SNS_LOGIN_SUCCESS:
+                case .SNS_LOGIN_SUCCESS:
                     let tutorialViewController = self.storyboard!.instantiateViewControllerWithIdentifier("timeLineEntry")
                     self.presentViewController(tutorialViewController, animated: true, completion: nil)
-                case SNSUtil.LoginResult.SNS_LOGIN_UNKNOWN_FAILURE:
+                case .SNS_LOGIN_UNKNOWN_FAILURE:
                     Util.popup("再ログインに失敗しました。アカウント情報を再度お確かめください。")
-                case SNSUtil.LoginResult.SNS_LOGIN_CANCELED:
+                case .SNS_LOGIN_CANCELED:
                     break
-                case SNSUtil.LoginResult.SNS_USER_NOT_REGISTERD:
+                case .SNS_USER_NOT_REGISTERD:
                     Util.popup("お使いのFacebookアカウントではまだGocciが登録されていません")
-                case SNSUtil.LoginResult.SNS_PROVIDER_FAIL:
+                case .SNS_PROVIDER_FAIL:
                     Util.popup("再ログインに失敗しました。Facebookが現在使用できません。大変申し訳ありません。")
             }
         }
     }
     
     @IBAction func twitterLoginClicked(sender: AnyObject) {
-        SNSUtil.singelton.loginWithTwitter(self) { (result) -> Void in
+        SNSUtil.loginWithTwitter(self) { (result) -> Void in
             print("Result: " + String(result))
             switch result {
-                case SNSUtil.LoginResult.SNS_LOGIN_SUCCESS:
+                case .SNS_LOGIN_SUCCESS:
                     let tutorialViewController = self.storyboard!.instantiateViewControllerWithIdentifier("timeLineEntry")
                     self.presentViewController(tutorialViewController, animated: true, completion: nil)
-                case SNSUtil.LoginResult.SNS_LOGIN_UNKNOWN_FAILURE:
+                case .SNS_LOGIN_UNKNOWN_FAILURE:
                     Util.popup("再ログインに失敗しました。アカウント情報を再度お確かめください。")
-                case SNSUtil.LoginResult.SNS_LOGIN_CANCELED:
+                case .SNS_LOGIN_CANCELED:
                     break
-                case SNSUtil.LoginResult.SNS_USER_NOT_REGISTERD:
+                case .SNS_USER_NOT_REGISTERD:
                     Util.popup("お使いのTwitterアカウントではまだGocciが登録されていません")
-                case SNSUtil.LoginResult.SNS_PROVIDER_FAIL:
+                case .SNS_PROVIDER_FAIL:
                     Util.popup("再ログインに失敗しました。Twitterが現在使用できません。大変申し訳ありません。")
 
             }
