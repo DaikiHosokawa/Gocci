7822682b312c2dc1f62fad2e694c267e9221321f
Markus Wanke <m-wanke@inase-inc.jp>
2015-11-18 15:34:08 +0900

    added test target again

---
+++ UnitTests/TestUtil.swift
@@ -0,0 +1,37 @@
+//
+//  TestUtil.swift
+//  Gocci
+//
+//  Created by Ma Wa on 18.11.15.
+//  Copyright © 2015 Massara. All rights reserved.
+//
+
+import XCTest
+
+class TestUtil: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testIfRandomUsernameHasCorrectFormat() {
+        let rus = Util.randomUsername()
+        XCTAssert(rus.length > 0, "username is empty string")
+        XCTAssert(rus.length < 20, "username is too long")
+    }
+    
+    
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+    
+}
