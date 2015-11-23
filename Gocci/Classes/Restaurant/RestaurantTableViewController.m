8dbc9920f99a835996b02c913945106fbaa3f796
Daiki Hosokawa <popeppo4@gmail.com>
2015-11-23 23:24:31 +0900

    A difference in the movie playing locations which occur every screen size is solved.

--- Gocci/Classes/Restaurant/RestaurantTableViewController.m
+++ Gocci/Classes/Restaurant/RestaurantTableViewController.m
@@ -513,7 +513,7 @@ static NSString * const SEGUE_GO_SC_RECORDER = @"goSCRecorder";
     TimelineCell *currentCell = [TimelineCell cell];
     [currentCell configureWithTimelinePost:self.posts[[self _currentIndexPath].row]];
     CGRect movieRect = CGRectMake((self.tableView.frame.size.width - currentCell.thumbnailView.frame.size.width) / 2,
-                                  currentHeight + currentCell.thumbnailView.frame.origin.y+430,
+                                  currentHeight + currentCell.thumbnailView.frame.origin.y+self.headerView.bounds.size.height,
                                   currentCell.thumbnailView.frame.size.width,
                                   currentCell.thumbnailView.frame.size.height);
     
