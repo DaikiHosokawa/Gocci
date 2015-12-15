05e1c775af02b4a2c4ddcde891224f2b459eebd7
Markus Wanke <m-wanke@inase-inc.jp>
2015-12-15 13:15:26 +0900

    Now full automatic compilation. different bundle identifier per build

--- Gocci/const.h
+++ Gocci/const.h
@@ -2,16 +2,15 @@
 //  const.h
 //  Gocci
 //
-//  Created by Sem on 9/4/15.
+//  Created by Markus Wanke on 9/4/15.
 //  Copyright (c) 2015 Massara. All rights reserved.
 //
 
-#ifndef Gocci_const_h
-#define Gocci_const_h
+#ifndef GOCCI_CONST_H
+#define GOCCI_CONST_H
 
-// #######################################################################################
-// Lots ob DEBUG improvments. TODO change this in the release version
-#define INDEVEL
+
+
 
 // #######################################################################################
 // The app will start with a debug screen to test logins etc.
@@ -25,7 +24,14 @@
 //#define ENTRY_POINT_JUMP (@"jumpSettingsTableViewController")
 //#define ENTRY_POINT_JUMP (@"jumpUsersViewController")
 
+// #######################################################################################
 
+// INDEVEL is gone now, please use:
+//#if TEST_BUILD
+//
+//#elif LIVE_BUILD
+//
+//#endif
 
 // #######################################################################################
 // #######################################################################################
@@ -33,61 +39,69 @@
 
 #define INASE_PRIVACY_URL (@"http://inase-inc.jp/rules/privacy/")
 #define INASE_RULES_URL (@"http://inase-inc.jp/rules/")
-
 #define GOCCI_APP_STORE_URL (@"itms://itunes.apple.com/jp/app/id968630887")
 
 
-#ifdef INDEVEL
-#define API_BASE_URL (@"http://test.api.gocci.me/v1/mobile/")
-#else
-#define API_BASE_URL (@"https://api.gocci.me/v1/mobile/")
+/// Gocci API 2
+#if TEST_BUILD
+    #define API_BASE_URL (@"http://test.api.gocci.me/v1/mobile/")
+    #define API_BASE_DOMAIN (@"test.api.gocci.me")
+    #define GOCCI_DEV_AUTH_PROVIDER_STRING (@"test.login.gocci")
+#elif LIVE_BUILD
+    #define API_BASE_URL (@"https://api.gocci.me/v1/mobile/")
+    #define API_BASE_DOMAIN (@"api.gocci.me")
+    #define GOCCI_DEV_AUTH_PROVIDER_STRING (@"login.gocci")
 #endif
 
-#ifdef INDEVEL
-#define API_BASE_DOMAIN (@"test.api.gocci.me")
-#else
-#define API_BASE_DOMAIN (@"api.gocci.me")
-#endif
-
-#ifdef INDEVEL
-#define GOCCI_DEV_AUTH_PROVIDER_STRING (@"test.login.gocci")
-#else
-#define GOCCI_DEV_AUTH_PROVIDER_STRING (@"login.gocci")
-#endif
-
-#ifdef INDEVEL
-#define AWS_S3_VIDEO_UPLOAD_BUCKET (@"gocci.movies.bucket.jp-test")
-#else
-#define AWS_S3_VIDEO_UPLOAD_BUCKET (@"gocci.movies.bucket.jp")
+/// AWS and Cognito
+#if TEST_BUILD
+    #define COGNITO_POOL_ID (@"us-east-1:b563cebf-1de2-4931-9f08-da7b4725ae35")
+    #define COGNITO_POOL_REGION (AWSRegionUSEast1)
+    #define COGNITO_POOL_REGION_SWIFT AWSRegionType.USEast1
+    #define AWS_S3_VIDEO_UPLOAD_BUCKET (@"gocci.movies.bucket.jp-test")
+#elif LIVE_BUILD
+    #define COGNITO_POOL_ID (@"us-east-1:b0252276-27e1-4069-be84-3383d4b3f897")
+    #define COGNITO_POOL_REGION (AWSRegionUSEast1)
+    #define COGNITO_POOL_REGION_SWIFT AWSRegionType.USEast1
+    #define AWS_S3_VIDEO_UPLOAD_BUCKET (@"gocci.movies.bucket.jp")
 #endif
 
 
-
-//#ifdef INDEVEL
+// Test Facebook I dont really want to use this anymore
 //#define FACEBOOK_APP_ID (@"148392115499214")
 //#define FACEBOOK_PROVIDER_STRING (@"graph.facebook.com")
 //#define FACEBOOK_STORY_ACTION_ID (@"gocci_test:record")
-//#else
+
 #define FACEBOOK_APP_ID (@"673123156062598")
 #define FACEBOOK_PROVIDER_STRING (@"graph.facebook.com")
 #define FACEBOOK_STORY_ACTION_ID (@"gocci:record")
-//#endif
 
 #define TWITTER_CONSUMER_KEY (@"kurJalaArRFtwhnZCoMxB2kKU")
 #define TWITTER_CONSUMER_SECRET (@"oOCDmf29DyJyfxOPAaj8tSASzSPAHNepvbxcfVLkA9dJw7inYa")
 #define TWITTER_PROVIDER_STRING (@"api.twitter.com")
 
-#ifdef INDEVEL
-#define COGNITO_POOL_ID (@"us-east-1:b563cebf-1de2-4931-9f08-da7b4725ae35")
-#define COGNITO_POOL_REGION (AWSRegionUSEast1)
-#define COGNITO_POOL_REGION_SWIFT AWSRegionType.USEast1
-#else
-#define COGNITO_POOL_ID (@"us-east-1:b0252276-27e1-4069-be84-3383d4b3f897")
-#define COGNITO_POOL_REGION (AWSRegionUSEast1)
-#define COGNITO_POOL_REGION_SWIFT AWSRegionType.USEast1
-#endif
-
 #define GOOGLE_MAP_SERVICE_API_KEY (@"AIzaSyDfZOlLwFm0Wv13lNgJF9nsfXlAmUTzHko")
 
 
-#endif // Gocci_const_h END
+
+
+
+
+
+
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////
+// Some protection for wrong scheme & compile configurations
+#ifndef TEST_BUILD
+#error "Please define TEST_BUILD and LIVE_BUILD as 1 and 0 and so that (LIVE_BUILD != LIVE_BUILD) == true"
+#endif
+
+#ifndef LIVE_BUILD
+#error "Please define TEST_BUILD and LIVE_BUILD as 1 and 0 and so that (LIVE_BUILD != LIVE_BUILD) == true"
+#endif
+
+#if LIVE_BUILD == TEST_BUILD
+#error "Please define TEST_BUILD and LIVE_BUILD as 1 and 0 and so that (LIVE_BUILD != LIVE_BUILD) == true"
+#endif
+
+#endif // GOCCI_CONST_H END
