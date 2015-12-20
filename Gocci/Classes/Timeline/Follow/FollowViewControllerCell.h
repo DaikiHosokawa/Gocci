a2f6ad36e649be72e6dbc69668e46a02532976d1
Daiki Hosokawa <popeppo4@gmail.com>
2015-12-20 19:39:59 +0900

    add profileImg to timeline page

--- Gocci/Classes/Timeline/Follow/FollowViewControllerCell.h
+++ Gocci/Classes/Timeline/Follow/FollowViewControllerCell.h
@@ -19,6 +19,7 @@
 
 - (void)followViewCell:(FollowViewControllerCell *)cell didTapThumb:(NSString*)rest_id;
 - (void)followViewCell:(FollowViewControllerCell *)cell didTapLikeButton:(NSString*)postID;
+- (void)followViewCell:(FollowViewControllerCell *)cell didTapImg:(NSString*)user_id;
 
 
 @end
@@ -28,7 +29,7 @@
     int flash_on;
 }
 
-
+@property (weak, nonatomic) IBOutlet UIImageView *profileImg;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UILabel *title;
 @property (weak, nonatomic) IBOutlet UILabel *date;
--- Gocci/Classes/Timeline/Follow/FollowViewControllerCell.m
+++ Gocci/Classes/Timeline/Follow/FollowViewControllerCell.m
@@ -53,6 +53,10 @@
                       placeholderImage:[UIImage imageNamed:@"dummy.1x1.#EEEEEE"]];
     self.imageView.layer.cornerRadius = 2;
     self.imageView.clipsToBounds = true;
+    [self.profileImg sd_setImageWithURL:[NSURL URLWithString:timelinePost.picture]
+                       placeholderImage:[UIImage imageNamed:@"dummy.1x1.#EEEEEE"]];
+    self.profileImg.layer.cornerRadius = 15;
+    self.profileImg.clipsToBounds = true;
     self.title.text = timelinePost.restname;
     self.date.text = timelinePost.timelabel;
     self.movieURL = timelinePost.movie;
@@ -62,6 +66,7 @@
     [self _assignTapAction:@selector(tapOption:) view:self.option];
     [self _assignTapAction:@selector(tapThumb:) view:self.imageView];
     [self _assignTapAction:@selector(tapLike:) view:self.likeBtn];
+     [self _assignTapAction:@selector(tapImg:) view:self.profileImg];
     
     NSString *string = [NSString stringWithFormat:@"%@", self.pushed_at];
     if ([string isEqualToString:@"0"])
@@ -110,6 +115,13 @@
                                                        completion:^(BOOL f){
                                                        }];
         [[MoviePlayerManager sharedManager] playMovieAtIndex:self.index inView:self.imageView  frame:self.imageView.frame];
+    }
+}
+
+- (void)tapImg:(UITapGestureRecognizer *)recognizer
+{
+    if ([self.delegate respondsToSelector:@selector(followViewCell:didTapImg:)]) {
+        [self.delegate followViewCell:self didTapImg:self.user_id];
     }
 }
 
