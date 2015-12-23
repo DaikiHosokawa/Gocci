1198cae09b589e35c6e249c63db7653cf85163a5
Daiki Hosokawa <popeppo4@gmail.com>
2015-12-23 21:49:35 +0900

    update Tutorial layout

--- Gocci/Classes/Login/TutorialPageViewController.m
+++ Gocci/Classes/Login/TutorialPageViewController.m
@@ -51,9 +51,16 @@
         [ruleButton addTarget:self action:@selector(ruleButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
     }
     
-    self.username = (UITextField *)[page3.view viewWithTag:3];
+    self.username = (JJMaterialTextfield *)[page3.view viewWithTag:3];
     self.username.returnKeyType = UIReturnKeyDone;
     self.username.delegate = self;
+    self.username.textColor=[UIColor whiteColor];
+    [self.username enableMaterialPlaceHolder:YES];
+    self.username.errorColor=[UIColor colorWithRed:0.910 green:0.329 blue:0.271 alpha:1.000]; // FLAT RED COLOR
+    self.username.font = [UIFont fontWithName:@"Helvetica-Bold" size:22];
+    self.username.lineColor=[UIColor colorWithRed:0.482 green:0.800 blue:1.000 alpha:1.000];
+    self.username.tintColor= [UIColor whiteColor];
+    self.username.placeholder=@"ユーザー名";
     
 #if TEST_BUILD
     self.username.text = [[[NSProcessInfo processInfo] globallyUniqueString] substringToIndex:8];
